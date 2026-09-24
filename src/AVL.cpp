b014a6e594c11e678c1aca4fb1d68a0a23ab055b
RyanXi11 <ryanxi57@gmail.com>
2026-09-23 20:26:26 -0400

    Completed rotation functions

--- src/AVL.cpp
+++ src/AVL.cpp
@@ -1,18 +1,68 @@
 #include "AVL.h"
 #include <iostream>
 
-int Node::getHeight(Node* node){
+Node *AVLTree::insertHelper(Node *node, string name, string ufid, bool &successful)
+{
+  return nullptr;
+}
+
+Node *AVLTree::removeHelper(Node *node, string ufid, bool &successful)
+{
+  return nullptr;
+}
+
+int AVLTree::getHeight(Node* node){
   if(node == nullptr){
     return 0;
   }
   return node->height;
 }
 
-int Node::getBalanceFactor(Node* node){
+int AVLTree::getBalanceFactor(Node* node){
   if(node == nullptr){
     return 0;
   }
   return getHeight(node->left) - getHeight(node->right);
+}
+
+Node* AVLTree::rotateLeft(Node *node){
+  Node* newRoot = node->right;
+  Node* displacedLeftChild = newRoot->left;
+
+  //Perform rotation
+  newRoot->left = node;
+  node->right = displacedLeftChild;
+
+  //Update heights
+  node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+  newRoot->height = 1 + max(getHeight(newRoot->left), getHeight(newRoot->right));
+  
+  return newRoot;
+}
+
+Node* AVLTree::rotateRight(Node *node){
+  Node* newRoot = node->left;
+  Node* displacedRightChild = newRoot->right;
+
+  // Perform rotation
+  newRoot->right = node;
+  node->left = displacedRightChild;
+
+  // Update heights
+  node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+  newRoot->height = 1 + max(getHeight(newRoot->left), getHeight(newRoot->right));
+
+  return newRoot;
+}
+
+Node* AVLTree::rotateLeftRight(Node *node){
+  node->left = rotateLeft(node->left);
+  return rotateRight(node);
+}
+
+Node* AVLTree::rotateRightLeft(Node *node){
+  node->right = rotateRight(node->right);
+  return rotateLeft(node);
 }
 
 bool AVLTree::insert(string name, string ufid){
@@ -25,18 +75,26 @@ bool AVLTree::remove(string ufid){
   return successful;
 }
 
-bool AVLTree::search(string ufid){
+bool AVLTree::removeInorder(int N){
+
+}
+
+bool AVLTree::searchID(string ufid){
   return false;
+}
+
+bool AVLTree::searchName(string name){
+
 }
 
 vector<string> AVLTree::getInorder(){
   return vector<string>();
 }
 
-Node* AVLTree::insertHelper(Node* node, string name, string ufid, bool& successful){
-  return nullptr;
+vector<string> AVLTree::getPreorder(){
+
 }
 
-Node* AVLTree::removeHelper(Node* node, string ufid, bool& successful){
-  return nullptr;
+vector<string> AVLTree::getPostorder(){
+
 }
--- src/AVL.h
+++ src/AVL.h
@@ -12,24 +12,43 @@ struct Node{
   Node* right;
 
   Node(string name, string ufid) : name(name), ufid(ufid), height(1), left(nullptr), right(nullptr) {}
-
-  int getHeight(Node* node);
-  int getBalanceFactor(Node* node);
 };
 
 class AVLTree{
   private:
     Node* root;
 
+    // Core recursive helpers
     Node* insertHelper(Node* node, string name, string ufid, bool& sucessful);
     Node* removeHelper(Node* node, string ufid, bool& sucessful);
+
+    // AVL mechanics and balance tracking
+    int getHeight(Node *node);
+    int getBalanceFactor(Node *node);
+
+    // Four standard AVL rotations
+    Node* rotateLeft(Node* node);
+    Node* rotateRight(Node* node);
+    Node* rotateLeftRight(Node* node);
+    Node* rotateRightLeft(Node* node);
 
   public: 
     AVLTree() : root(nullptr) {}
 
+    //Core commands
     bool insert(string name, string ufid);
     bool remove(string ufid);
-    bool search(string ufid);
+    bool removeInorder(int N);
 
+    // Search commands
+    bool searchID(string ufid);
+    bool searchName(string name);
+
+    //Traversal commands
     vector<string> getInorder();
+    vector<string> getPreorder();
+    vector<string> getPostorder();
+
+    // Utility
+    int getLevelCount();
 };
